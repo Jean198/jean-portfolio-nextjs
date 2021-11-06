f3de7139c78b8df7a78b4d51fd2793d1c224b79a
Jean198 <jeandedieu.niyigaba@gmail.com>
2021-11-06 22:37:48 +0200

    m

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,12 +11,16 @@ export default function NavBar() {
                     </a>
                 </Link>
 
+                <Link href="/" >
+                    <a className="navbar-links">Home</a>
+                </Link>
+
                 <Link href="/about">
                     <a className="navbar-links">About Me</a>
                 </Link>
 
                 <Link href="https://jeans-journal.herokuapp.com/">
-                    <a className="navbar-links">Blog</a>
+                    <a className="navbar-links" target="blank">Blog</a>
                 </Link>
                 
             </div>
