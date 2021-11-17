4f0d627f90c4b1349e0bfbf39478de6cc5e26753
Jean198 <jeandedieu.niyigaba@gmail.com>
2021-11-17 21:21:52 +0200

    edited currency app link

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -3,6 +3,8 @@ import Image from "next/image"
 import logo from "../public/images/logo-iso.png";
 import bitcoin from "../public/images/bitcoin.png"
 import weather from "../public/images/weather.png"
+import currency from "../public/images/currency.png"
+import bit from "../public/images/bit.png"
 
 export default function Projects() {
   return (
@@ -13,7 +15,7 @@ export default function Projects() {
       </div>
       
         <div className="project-box">
-          <h2>Softrain project (<span className="frameworks"> React/Node js </span>)</h2>
+          <h2>Softrain project (<span className="frameworks"> React/Next js/Tailwind CSS </span>)</h2>
           <a href=""><FaGithub className="text-2xl" /></a>
           <p>
           While working as trainee software developer at Softrain Blobs Oy, my main task is to rewrite the logitar software using React
@@ -31,19 +33,19 @@ export default function Projects() {
           </p>
         </div>
         <div className="project-box">
-        <h2>Crypto-currency tracker App (<span className="frameworks"> Next Js </span>)</h2>
+        <h2>Crypto-currency tracker App (<span className="frameworks"> React/Next js </span>)</h2>
         <a href=""><FaGithub className="text-2xl" /></a>
           <p>
             This app built with Next Js gives current overall information about crypto-currency values on the market.
 
             <div className="project-image-div">
-              <Image className="image" src={bitcoin} alt="Here should be image"  />
+              <Image className="image" src={bit} alt="Here should be image"  />
 
 
             </div>
             
               <span className="span"> 
-                <a href="">
+                <a href="https://crypto-currency-tracker-tau.vercel.app/">
                   <button className="project-button">Go to the App</button>
                 </a>
               </span> 
@@ -52,7 +54,7 @@ export default function Projects() {
           </p>
         </div>
         <div className="project-box">
-        <h2>Weather App (<span className="frameworks"> Node/express </span>)</h2>
+        <h2>Currency Converter (<span className="frameworks"> Node js/express </span>)</h2>
         <a href=""><FaGithub className="text-2xl" /></a>
           <p>
           survived not only five centuries, but also the leap into electronic typesetting,
@@ -62,12 +64,12 @@ export default function Projects() {
 
            <div className="project-image-div">
 
-              <Image className="image" src={weather} alt="Here should be image"  />
+              <Image className="image" src={currency} alt="Here should be image"  />
 
            </div>
 
            <span className="span"> 
-                <a href="">
+                <a href="https://jn-currency-converter.herokuapp.com/">
                   <button className="project-button">Go to the App</button>
                 </a>
            </span> 
