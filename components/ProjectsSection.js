61d8eb7c49eb5b56a25be1fc7d9314fbdaaada67
Jean198 <jeandedieu.niyigaba@gmail.com>
2021-11-18 13:05:07 +0200

    changed images

--- components/ProjectsSection.js
+++ components/ProjectsSection.js
@@ -86,7 +86,7 @@ export default function Projects() {
 
 
           <p>
-          survived not only five centuries, but also the leap into electronic typesetting,
+          Lorem not only five centuries, but also the leap into electronic typesetting,
            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions 
            of Lorem Ipsum.
