25ee6da36485fdb1cb656f920ec1a3eb4739ce20
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-04-13 22:09:51 +0300

    added references

--- components/References.js
+++ components/References.js
@@ -7,39 +7,46 @@ import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 export default function References() {
     return (
           
-                <div className="References">
+                <div >
+                    <div className="projects-title">
+                        <h3>References</h3>
+                    </div>
+                    <div className="References">
+                    <div className="projects-title">
+                            
+                            </div>
+                       
+                            <div>
+                                <h3 className="reference-name">Esko Ronkainen</h3><span> (CEO at Softrain Blobs Oy)</span>
+    
+                                <p>Email: esko.ronkainen@softrain.fi</p>
+                                
+                                
+                                <p><a className="letter-of-recommendation"
+                                    href="/files/Jeanharjoittelutodistus_allekirjoittu.pdf"
+                                    alt="alt text"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >See my Letter of Recommendation from Softrain Blobs</a></p>
+                            </div> <br /> <br />
+                            <div>
+                                <h3 className="reference-name">Jouni Huotari</h3><span> (Principal lecturer at JAMK and CEO at Nestronite)</span>
+                                <p>Email: jouni.huotari@jamk.fi </p>
+                                <p>Tel: +358408344615</p>
+                                
+                                
+                            </div> <br /><br />
+                            <div>
+                                <h3 className="reference-name">Matti Mieskolainen</h3><span> ( Lehtori, Senior Lecturer at JAMK)</span>
+                                <p>Email: matti.mieskolainen@jamk.fi</p>
+                                <p>Tel: +358400444781</p>
+                                
+                                
+                            </div>
 
-                        <div className="projects-title">
-                            <h3>References</h3>
-                        </div>
-                   
-                        <div>
-                            <h3 className="reference-name">Esko Ronkainen</h3><span> (CEO at Softrain Blobs Oy)</span>
+                    </div>
 
-                            <p>Email:</p>
-                            <p>Tel:</p> <br />
-                            
-                            <p><a className=""
-                                href="/files/Jeanharjoittelutodistus_allekirjoittu.pdf"
-                                alt="alt text"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >Letter of Recommendation</a></p>
-                        </div> <br /> <br />
-                        <div>
-                            <h3 className="reference-name">Jouni Huotari</h3><span> (Principal lecturer at JAMK and CEO at Nestronite)</span>
-                            <p>Email:</p>
-                            <p>Tel:</p>
-                            
-                            
-                        </div> <br /><br />
-                        <div>
-                            <h3 className="reference-name">Matti Mieskolainen</h3><span> ( Lehtori, Senior Lecturer at JAMK)</span>
-                            <p>Email:</p>
-                            <p>Tel:</p>
-                            
-                            
-                        </div>
+                        
                     
                 </div>
                    
